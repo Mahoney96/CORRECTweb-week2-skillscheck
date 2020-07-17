f513ba6e2cd92f46b8c6ad0780391a12de56d104
Mahoney96 <cmahoney966@gmail.com>
2020-07-17 08:52:11 -0700

    initial save to check correct file

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -4,11 +4,15 @@
 
 // Code here
 
+let myName = 'Cam';
+
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' with a value that is your favorite number. 
 
 //Code here
+
+let  faveNum = 27;
 
 //////////////////PROBLEM 3////////////////////
 
@@ -16,17 +20,32 @@
 
 //Code here
 
+let lovesCode = true;
+
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. The function should return the sum of the two parameters.
 
 //Code here
 
+function sum(num1, num2){
+	return num1 + num2;
+}
+
+sum(5,5);
+
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called 'x'. Check to see if 'x' is equal to true. If it is, return a true boolean. If 'x' does not equal true, return a false boolean.  
 
 //Code here
+
+function lovesCode(x){
+	return if( x === true){
+		return true;
+	}
+}
+
 
 //////////////////PROBLEM 6////////////////////
 
